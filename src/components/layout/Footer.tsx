c083b7bc0c3994e4bfa5c1a1cbdfec722a80754e
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-02-01 19:51:32 +0900

    静的ページ作成

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="text-center shrink-0">Todo App with Comments</footer>;
+};
+
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="shrink-0 px-4 py-6 flex items-center gap-5 shadow-md">
+      <h1 className="grow text-center">Todo App with Comments</h1>
+      <Button>New Todo</Button>
+    </header>
+  );
+};
+
+export default Header;
