ee06e2574e4f7fe0b840dc51e9ee44d4f4d229dd
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-02-02 18:32:46 +0900

    タイポミス

--- supabase/migrations/20260202050845_create_todos.sql
+++ supabase/migrations/20260202050845_create_todos.sql
@@ -3,7 +3,7 @@ create table if not exists public.todos (
  id uuid primary key default gen_random_uuid(),
  title text not null,
  contents text null,
- status todos_status nut null default 'TODO',
+ status todos_status not null default 'TODO',
  due_date date not null default current_date,
  created_at timestamptz not null default now(),
  updated_at timestamptz not null default now()
