4c86026ed6aa382a394eec97d339a2e2dd31ebd2
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-02-02 18:07:30 +0900

    todosをposgreのsqlで定義。 レビュー時のcodexからの指摘方法定義

---
+++ supabase/migrations/20260202050845_create_todos.sql
@@ -0,0 +1,10 @@
+create type todos_status as enum ('TODO','PROGRESS','COMPLETE')
+create table if not exists public.todos (
+ id uuid primary key default gen_random_uuid(),
+ title text not null,
+ contents text null,
+ status todos_status default 'TODO',
+ schedule date not null default current_date,
+ createdAt date not null default current_date,
+ updatedAt date not null default current_date,
+)
