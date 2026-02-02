27a035616af579c37c3ff5f6d818dd5ee2f9252e
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-02-02 20:32:03 +0900

    commentsテーブルの作成

---
+++ supabase/migrations/20260202112325_create_comments.sql
@@ -0,0 +1,7 @@
+create table if not exists public.comments {
+  id uuid primary key default gen_random_uuid(),
+  contents text not null,
+  todo_id uuid not null references todos(id),
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+}
