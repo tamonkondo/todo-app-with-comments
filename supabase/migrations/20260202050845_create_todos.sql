87b86809f34c50bfef0d0bc1cb8668799975b126
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-02-02 18:12:33 +0900

    create文にコロンを追加

--- supabase/migrations/20260202050845_create_todos.sql
+++ supabase/migrations/20260202050845_create_todos.sql
@@ -1,4 +1,4 @@
-create type todos_status as enum ('TODO','PROGRESS','COMPLETE')
+create type todos_status as enum ('TODO','PROGRESS','COMPLETE');
 create table if not exists public.todos (
  id uuid primary key default gen_random_uuid(),
  title text not null,
@@ -7,4 +7,4 @@ create table if not exists public.todos (
  schedule date not null default current_date,
  createdAt date not null default current_date,
  updatedAt date not null default current_date,
-)
+);
